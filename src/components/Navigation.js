5557b6d1a4efc0982f0633ed80f519062d56f739
vesnastokic <vesnastokic019@gmail.com>
2024-03-18 17:57:06 -0500

    I have made some changes

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
---
+++ src/components/ThankYou.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import logo from '../logo.svg';
+
+const ThankYou = () => {
+  return (
+    <div style={{ marginTop: '50px' }}>
+      <img src={logo} alt="logo" style={{ width: '100px', height: '100px' }} />
+      <h1 style={{ fontSize: '24px' }}>Thank you for being part of our mission. Design is not just what it looks like and feels like. Design is how it works. SJ!</h1>
+      
+    </div>
+  );
+}
+
+export default ThankYou;
